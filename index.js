04451f83ce55c413c5596e27ed45f49a1541bd13
Otieno Harrison Omondi <ogaloharrison@gmail.com>
2022-06-13 01:03:25 +0300

    commit tree d93758ddf42d922297d74737c1e695d3af013a3c

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    const driversArray = drivers.filter(
+        (match) => match.toLowerCase() === name.toLowerCase());
+    return driversArray;
+}
+
+function fuzzyMatch(drivers, name) {
+  const driversArray = drivers.filter(
+      (match) => match.toLowerCase().indexOf(name.toLowerCase()) === 0
+  )
+  return driversArray;
+}
+
+function matchName(driver, name) {
+  const driverArray = driver.filter(
+      (match) => match.name === name
+  )
+  return driverArray;
+}
